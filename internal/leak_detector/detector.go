769ca67218e129a777ccb3e0e2a5ae0981e8d199
FadyGamilM <gamilfady605@gmail.com>
2025-08-08 21:40:53 +0300

    enhance the leak detector to store prev snapshots with consecutive checker not just the latest snapshot

---
+++ internal/leak_detector/detector.go
@@ -0,0 +1,56 @@
+package leakdetector
+
+import (
+	"context"
+
+	"github.com/fadygamilm/go-leak-detector/internal/parser"
+)
+
+// simply this pkg will track the go routines from the current run to the previous run, and if we found the same goroutine in the previous snapshot run with same status, we will consider it a lean (a stuck go routine) and we will report it.
+type LeakDetector struct {
+	ctx           context.Context
+	PrevSnapshots map[string][]parser.GoroutineStackReport
+	LeakThreshold int
+}
+
+func New(ctx context.Context, leakThreshold int) *LeakDetector {
+	return &LeakDetector{
+		ctx:           ctx,
+		PrevSnapshots: make(map[string][]parser.GoroutineStackReport),
+		LeakThreshold: leakThreshold,
+	}
+}
+
+func (ld *LeakDetector) DetectLeaks(goroutinesReports []parser.GoroutineStackReport) []parser.GoroutineStackReport {
+	leakedRoutines := []parser.GoroutineStackReport{}
+	currentSnapshot := make(map[string]parser.GoroutineStackReport, len(goroutinesReports))
+
+	for _, report := range goroutinesReports {
+		currentSnapshot[report.Id] = report
+		ld.PrevSnapshots[report.Id] = append(ld.PrevSnapshots[report.Id], report)
+
+		if len(ld.PrevSnapshots[report.Id]) >= ld.LeakThreshold {
+			isLeak := true // its a leaked routine unless it's proven to be not so
+			for i := 1; i < len(ld.PrevSnapshots[report.Id]); i++ {
+				// so once i will find a 2 consutive reports with different status, i will consider it not a leak
+				if ld.PrevSnapshots[report.Id][i].Status != ld.PrevSnapshots[report.Id][i-1].Status {
+					isLeak = false
+					break
+				}
+			}
+			if isLeak {
+				leakedRoutines = append(leakedRoutines, report)
+			}
+		}
+	}
+
+	// i will clean the previous snapshots to keep only the latest snapshot found in the current to avoid memory leaks if the program runs for a long time
+	for id := range ld.PrevSnapshots {
+		if _, exists := currentSnapshot[id]; !exists {
+			delete(ld.PrevSnapshots, id)
+		}
+
+	}
+
+	return leakedRoutines
+}
